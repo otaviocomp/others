6798789c686d4c98d2421494b55ae41a65c8dd39
otaviocomp <32240652+otaviocomp@users.noreply.github.com>
2018-02-18 07:08:57 -0300

    example ncurses

---
+++ example_ncurses.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+
+int main()
+{
+	initscr();
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_BLUE, COLOR_WHITE);
+	init_pair(3, COLOR_RED, COLOR_WHITE);
+
+	bkgd(COLOR_PAIR(1));
+	attron(COLOR_PAIR(3));
+	move(2, 1);
+	printw("hello world!");
+	attroff(COLOR_PAIR(3));
+	move(3, 1);
+	printw("press any key to exit.");
+
+	attroff(COLOR_PAIR(2));
+	refresh();
+	getch();
+
+	endwin();
+}
+	
