126789b15deab1d9c46aee615caa2e02ce83e1b1
otaviocomp <otavio10espsanto@gmail.com1>
2018-04-22 14:13:35 +0000

    update

--- atividadeRob/atividade.cpp
+++ atividadeRob/atividade.cpp
@@ -8,21 +8,20 @@ int main(){
 	Mat image, logo, grayImage, contorno, contorno1, contorno2, laplacian, prewittX, prewittY, result;
 	VideoCapture cap;
 
-	//filtro para deteccao de bordas
-	laplacian = (Mat_<float>(3, 3) << 1, 1, 1, 1, -8, 1, 1, 1, 1);
+	//mascaras dos filtros para deteccao de bordas
 	prewittX = (Mat_<float>(3, 3) << 1, 1, 1, 0, 0, 0, -1, -1, -1);
 	prewittY = (Mat_<float>(3, 3) << 1, 0, -1, 1, 0, -1, 1, 0, -1);
 
 	cap.open(0); //liga a camera
 	cap >> image; //captura a imagem
 	cvtColor(image, grayImage, CV_BGR2GRAY); //converte para imagem em preto e branco
-	filter2D(grayImage, contorno, -1, laplacian);
-	filter2D(grayImage, contorno1, -1, prewittX);
-	filter2D(grayImage, contorno2, -1, prewittY);
-	cvtColor(contorno, image, COLOR_GRAY2BGR); //converte para imagem colorida
-	logo = imread("image.png", CV_LOAD_IMAGE_COLOR);
-	//addWeighted(contorno1, 1, contorno2, 1, 0.0, contorno);
-	//logo.copyTo(contorno(Rect(image.cols - logo.cols, image.rows - logo.rows ,logo.cols, logo.rows)));
-    imshow("image", image);
+	filter2D(grayImage, contorno1, -1, prewittX); //filtro prewitt em X
+	filter2D(grayImage, contorno2, -1, prewittY); //filtro prewitt em Y
+	logo = imread("logo.jpg", 0);
+	addWeighted( contorno1, 1, contorno2, 1, 0.0, contorno);
+	
+	//cola a logo e exibe o resultado
+	logo.copyTo(contorno(Rect(contorno.cols - logo.cols, contorno.rows - logo.rows ,logo.cols, logo.rows)));
+    imshow("image", contorno);
 	waitKey();
 }
